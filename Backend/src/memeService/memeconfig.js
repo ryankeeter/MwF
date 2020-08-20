669d881e927eaead93649a7c71bd2f0e6171c2cc
Ryan J. Keeter <ryan.keeter@gmail.com>
2020-08-20 15:47:01 -0500

    stored away the secret

--- Backend/src/memeService/memeconfig.js
+++ Backend/src/memeService/memeconfig.js
@@ -5,6 +5,5 @@ module.exports = {
   THUNDERDUNGEON_RSS: "https://thunderdungeon.com/feed",
   _9GAG: "https://9gag.com/trending",
   THECHIVE_RSS: "https://feeds.feedburner.com/feedburner/ZdSV",
-
 };
 
